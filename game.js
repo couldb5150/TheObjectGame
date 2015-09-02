f3bb34d4b31d1e0e644b0f4acdfd010144931334
Nathan Morin <natemorin17@gmail.com>
2015-09-01 19:27:59 -0600

    no message

--- game.js
+++ game.js
@@ -1,9 +1,27 @@
 var health=100;
+var name="Jake";
+var hits=0;
+
 function slap(){
 	health-=1;
+	hits++;
+	update();
+	}
+function punch(){
+	health-=5;
+	hits++;
+	update();
+	}
+function kick(){
+	health-=10;
+	hits++;
 	update();
 	}
 function update(){
 var hp = document.getElementById("health");
 hp.innerText = health;
+var name1 = document.getElementById("name");
+name1.innerText = name;
+var hits1 = document.getElementById("hits");
+hits1.innerText = hits;
 }
