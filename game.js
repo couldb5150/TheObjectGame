7552aeb741bb9f768bb34e49029ff88358dee437
Adam Stokes <adam.stokes13@yahoo.com>
2015-09-01 21:11:18 -0600

    no message

--- game.js
+++ game.js
@@ -1,33 +1,46 @@
-var health=100;
-var name="Jake";
-var hits=0;
 
-function slap(){
-	health-=1;
-	hits++;
-	update();
-	}
-function punch(){
-	health-=5;
-	hits++;
-	update();
-	}
-function kick(){
-	health-=10;
-	hits++;
-	update();
-	}
-function reset() {
-	hits=0;
-	health=100;
-	update();
+//var health=100;
+//var name="Jake";
+//var hits=0;
+
+var player =
+{
+	name:"Steve",
+	health:100,
+	hits:0,
+	
+	slap: function (){
+		this.health-=1;
+		this.hits++;
+		update();
+	},
+	punch: function (){
+		this.health-=5;
+		this.hits++;
+		update();
+	},
+	kick: function (){
+		this.health-=10;
+		this.hits++;
+		update();
+	},
+	reset: function (){
+		this.hits=0;
+		this.health=100;
+		update();
 }
+	
+}
+
+
+
+
 function update(){
 var hp = document.getElementById("health");
-hp.innerText = health;
+hp.innerText = player.health;
 var name1 = document.getElementById("name");
-name1.innerText = name;
+name1.innerText = player.name;
 var hits1 = document.getElementById("hits");
-hits1.innerText = hits;
+hits1.innerText = player.hits;
 }
 
